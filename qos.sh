97d4a253597490a32bdd37b1b0430051d494d1ec
root <root@node-0.junjieqc-qv19173.networklatency-pg0.utah.cloudlab.us>
2016-10-16 21:11:03 -0600

    data min 4G

--- qos.sh
+++ qos.sh
@@ -5,12 +5,9 @@ ovs-vsctl set port eth1 qos=@newqos -- \
            other-config:max-rate=10000000000 \
            queues:111=@q0 \
            queues:222=@q1 -- \
-       --id=@q0 create queue other-config:min-rate=1000000000 other-config:max-rate=1000000000 -- \
-       --id=@q1 create queue other-config:min-rate=1000000000 other-config:max-rate=1000000000
+       --id=@q0 create queue other-config:min-rate=4000000000 other-config:max-rate=10000000000 -- \
+       --id=@q1 create queue other-config:min-rate=4000000000 other-config:max-rate=10000000000
 
 
-ovs-ofctl add-flow br0 in_port=5,actions=set_queue:111,normal
-ovs-ofctl add-flow br0 in_port=6,actions=set_queue:222,normal
-
-ovs-vsctl set interface vport1 ingress_policing_rate=100000
-ovs-vsctl set interface vport2 ingress_policing_rate=100000
+ovs-ofctl add-flow br0 "nw_dst=10.10.1.11 actions=set_queue:111,normal"
+ovs-ofctl add-flow br0 "nw_dst=10.10.1.12 actions=set_queue:222,normal"
--- sockperf.sh
+++ sockperf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-iperf -c 10.10.1.11 -t 60 1> iperf.txt &
-iperf -c 10.10.1.12 -t 60 1> iperf.txt &
+iperf -c 10.10.1.11 -t 60 1> iperf1_$1.txt &
+iperf -c 10.10.1.12 -t 60 1> iperf2_$1.txt &
 (sleep 25s &&
-sockperf pp -i 10.10.1.11 -t 30 -p 8899 --tcp 1>sockperf1.txt) &
+sockperf pp -i 10.10.1.11 -t 30 -p 8899 --tcp 1>sockperf1.txt --full-log /users/Junjieqc/ovs/sockperf1_$1.log) &
 (sleep 25s &&
-sockperf pp -i 10.10.1.12 -t 30 -p 8899 --tcp 1>sockperf2.txt)
+sockperf pp -i 10.10.1.12 -t 30 -p 8899 --tcp 1>sockperf2.txt --full-log /users/Junjieqc/ovs/sockperf2_$1.log)
  
