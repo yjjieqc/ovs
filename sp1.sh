30fd4f387500202fd34f91131c57a1a24dbff17a
root <root@node-0.newex.networklatency-pg0.wisc.cloudlab.us>
2016-10-18 03:28:57 -0500

    experiment no.1 data

--- sp1.sh
+++ sp1.sh
@@ -2,7 +2,7 @@
 
 iperf -c 10.10.1.2 -t 60 1>./oneiperf/iperf1_$1.txt &
 iperf -c 10.10.1.3 -t 60 1>./oneiperf/iperf2_$1.txt &
-(sleep 25s &&
-sockperf pp -i 10.10.1.2 -t 30 -p 8899 --tcp --pps=100 1>sockperf1.txt --full-log /users/Junjieqc/ovs/sockperf1_$1.log) &
-(sleep 25s &&
-sockperf pp -i 10.10.1.3 -t 30 -p 8899 --tcp --pps=100 1>sockperf2.txt --full-log /users/Junjieqc/ovs/sockperf2_$1.log)
+(sleep 25s 
+sockperf pp -i 10.10.1.2 -t 30 -p 8899 --tcp --pps=100 --full-log /users/Junjieqc/ovs/sockperf1_$1.log) &
+(sleep 25s 
+sockperf pp -i 10.10.1.3 -t 30 -p 8899 --tcp --pps=100 --full-log /users/Junjieqc/ovs/sockperf2_$1.log)
