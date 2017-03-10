b0ee760ba5a2e10de2b01e7da72a1722c61cc077
yjjieqc <lonelypiscesve@gmail.com>
2017-03-10 11:24:58 +0800

    Update sp1.sh

--- sp1.sh
+++ sp1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-iperf -c 10.10.1.2 -t 20 1 > iperf-$1.txt &
+iperf -c 10.10.1.2 -t 20 -i 1 > iperf-$1.txt &
 (sleep 5s 
 sockperf pp -i 10.10.1.2 -t 10 -p 8899 --tcp --pps=100 --full-log sockperf-$1.log) 
