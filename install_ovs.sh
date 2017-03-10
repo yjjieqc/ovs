345a9117c00ba5f66f1f4f97c2f2809a09c23279
yjjieqc <lonelypiscesve@gmail.com>
2017-03-10 11:02:14 +0800

    Update install_ovs.sh

--- install_ovs.sh
+++ install_ovs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget http://openvswitch.org/releases/openvswitch-2.5.0.tar.gz
-tar xzvf openvswitch-2.5.0.tar.gz 
-cd openvswitch-2.5.0/
+wget http://openvswitch.org/releases/openvswitch-2.6.0.tar.gz
+tar xzvf openvswitch-2.6.0.tar.gz 
+cd openvswitch-2.6.0/
 apt-get install -y git automake autoconf gcc uml-utilities libtool build-essential pkg-config linux-headers-`uname -r`
 apt-get -y install libnl-dev
 ./boot.sh
