514a88c3c0424832a904d8880684324656364b1c
yjjieqc <lonelypiscesve@gmail.com>
2017-03-10 11:23:45 +0800

    Update sp1.sh

--- sp1.sh
+++ sp1.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-iperf -c 10.10.1.2 -t 60 1>./oneiperf/iperf1_$1.txt &
-iperf -c 10.10.1.3 -t 60 1>./oneiperf/iperf2_$1.txt &
-(sleep 25s 
-sockperf pp -i 10.10.1.2 -t 30 -p 8899 --tcp --pps=100 --full-log /users/Junjieqc/ovs/sockperf1_$1.log) &
-(sleep 25s 
-sockperf pp -i 10.10.1.3 -t 30 -p 8899 --tcp --pps=100 --full-log /users/Junjieqc/ovs/sockperf2_$1.log)
+iperf -c 10.10.1.2 -t 20 1 > iperf-$1.txt &
+(sleep 5s 
+sockperf pp -i 10.10.1.2 -t 10 -p 8899 --tcp --pps=100 --full-log sockperf-$1.log) 
