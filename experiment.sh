152069836b560bc786b794130637d3ef917a2f8f
yjjieqc <lonelypiscesve@gmail.com>
2017-03-14 10:35:04 +0800

    Update experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
+for i in $(seq 10000 10000 90000)
+do
+t=$((t=$i+10000))
 for j in 500 1000 2000
+do
+for k in 1024 2048 4096
 do
 cd /users/Junjieqc/openvswitch-2.6.0
 tc qdisc del dev eth2 root
 ./remove_ovs.sh
-insmod datapath/linux/openvswitch.ko threshold1=$1 threshold2=$2 ALPHA=$3
+insmod datapath/linux/openvswitch.ko threshold1=$i threshold2=$t ALPHA=$k
 ./ovs_config.sh
 ./ovs_port.sh
 ./rate_config.sh "$j"mbit
 cd /users/Junjieqc/ovs
-for i in 1 2 3 4 5
-do 
 iperf -c 10.10.1.2 -t 20 -i 1 > iperf-$j-$i.txt &
 (sleep 5s
 sockperf pp -i 10.10.1.2 -t 10 -p 8899 --tcp --pps=100 --full-log sockperf-$j-$i.log)
 sleep 3s
-echo finish $i
-
+echo finish delta $k
 done
-echo finish $j mbit
+echo finish $j
+done
+echo finish threshold $i $j mbit
 done
