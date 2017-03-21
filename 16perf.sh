8690b4366fba40e9651f9ea20c7f6e86a976dc12
root <root@node-0.dawn.networklatency-pg0.wisc.cloudlab.us>
2017-03-20 20:51:14 -0500

    modified 16 iperf scripit

---
+++ 16perf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+(iperf -c 10.10.1.11 -t 20 -i 10 -P  8 > iperf1to11.txt & 
+iperf -c 10.10.1.12 -t 20 -i 10 -P 8 > iperf1to12.txt) & 
+(sleep 5s  && 
+(sockperf pp -i 10.10.1.11 -t 10 -p 8899 --tcp --pps=100 --full-log sockperf1to11.log & 
+sockperf pp -i 10.10.1.12 -t 10 -p 8899 --tcp --pps=100 --full-log sockperf1to12.log))
+
+
