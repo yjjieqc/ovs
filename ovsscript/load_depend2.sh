799d15e9dd6648a181b2e78eae09e3405f6ff76a
yjjieqc <lonelypiscesve@gmail.com>
2017-05-22 15:15:28 +0800

    Update load_depend2.sh

--- ovsscript/load_depend2.sh
+++ ovsscript/load_depend2.sh
@@ -1,7 +1,8 @@
-sudo insmod /lib/modules/4.4.0-62-generic/kernel/lib/libcrc32c.ko
-sudo insmod /lib/modules/4.4.0-62-generic/kernel/net/ipv4/udp_tunnel.ko
-sudo insmod /lib/modules/4.4.0-62-generic/kernel/net/netfilter/nf_conntrack.ko
-sudo insmod /lib/modules/4.4.0-62-generic/kernel/net/netfilter/nf_nat.ko
-sudo insmod /lib/modules/4.4.0-62-generic/kernel/net/ipv6/netfilter/nf_defrag_ipv6.ko
-sudo insmod /lib/modules/4.4.0-62-generic/kernel/net/ipv4/netfilter/nf_nat_ipv4.ko
-sudo insmod /lib/modules/4.4.0-62-generic/kernel/net/ipv6/netfilter/nf_nat_ipv6.ko
+sudo insmod /lib/modules/4.6.0-custom/kernel/lib/libcrc32c.ko
+sudo insmod /lib/modules/4.6.0-custom/kernel/net/ipv4/udp_tunnel.ko
+sudo insmod /lib/modules/4.6.0-custom/kernel/net/netfilter/nf_conntrack.ko
+sudo insmod /lib/modules/4.6.0-custom/kernel/net/netfilter/nf_nat.ko
+sudo insmod /lib/modules/4.6.0-custom/kernel/net/ipv6/netfilter/nf_defrag_ipv6.ko
+sudo insmod /lib/modules/4.6.0-custom/kernel/net/ipv4/netfilter/nf_nat_ipv4.ko
+sudo insmod /lib/modules/4.6.0-custom/kernel/net/ipv6/netfilter/nf_nat_ipv6.ko
+sudo insmod /lib/modules/4.6.0-custom/kernel/net/sched/sch_htb.ko printfrequency=10000
